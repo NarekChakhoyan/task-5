5448495fabb6bbb34fe247e8b315af831138a7bd
Narek Chakhoyan <narek.chakhoyan.g@gmail.com>
2023-08-31 15:35:01 +0400

    first commit

---
+++ script2.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let a = 5;
+
+let b = 7;
+let d = 7;
+
+function fn() {
+    a + b
+}
+
+function fnn() {
+    a + b
+}
+
+let c = true;
+
+let f = false;
+
+function fun() {
+    b - a
+}
+
+let a1 = 5;
+
+let b1 = 7;
+
+function fnnn() {
+    a + b
+}
